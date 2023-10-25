cf6bff61cfae8d6a20072cf378fd86ae0ef7ec45
levin979 <magoludwing@gmail.com>
2023-10-25 13:16:38 -0600

    Showing porducts on main page and adding calls to DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import HeroCarousel from "@/components/HeroCarousel"
+import ProductCard from "@/components/ProductCard"
 import Searchbar from "@/components/Searchbar"
+import { getAllProducts } from "@/lib/actions"
 import Image from "next/image"
 
-const Home = () => {
+const Home = async () => {
+
+  const allProducts = await getAllProducts()
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -34,8 +39,8 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['apple iphone 15', 'book', 'nike'].map(product => (
-            <div>{product}</div>
+          {allProducts?.map(product => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
